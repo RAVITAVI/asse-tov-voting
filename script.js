66fe438f88c5748d3255598ca1590325d01e9fe3
RAVITAVI <135533909+RAVITAVI@users.noreply.github.com>
2026-05-26 08:18:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const openingScreen = document.getElementById('opening-screen');
 const genderScreen = document.getElementById('gender-screen');
 const schoolScreen = document.getElementById('school-screen');
 
-let selectedGender = "";
+let userGender = ""; // כאן יישמר הקוד (male/female)
 
 document.getElementById('startBtn').addEventListener('click', () => {
     openingScreen.classList.remove('active');
@@ -10,13 +10,15 @@ document.getElementById('startBtn').addEventListener('click', () => {
 });
 
 document.getElementById('boyBtn').addEventListener('click', () => {
-    selectedGender = "boy";
+    userGender = "male";
+    console.log("המגדר שנבחר:", userGender); // לבדיקה
     genderScreen.classList.remove('active');
     schoolScreen.classList.add('active');
 });
 
 document.getElementById('girlBtn').addEventListener('click', () => {
-    selectedGender = "girl";
+    userGender = "female";
+    console.log("המגדר שנבחר:", userGender); // לבדיקה
     genderScreen.classList.remove('active');
     schoolScreen.classList.add('active');
 });
