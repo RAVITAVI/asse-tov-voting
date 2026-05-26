a1b5b6c90592b73eaf7227707cb728d69ff5e1f6
RAVITAVI <135533909+RAVITAVI@users.noreply.github.com>
2026-05-26 15:46:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const schoolDropdown = document.getElementById('school-dropdown');
 const nextBtn = document.getElementById('nextBtn');
 
 // 1. הדביקי כאן את הקישור הרגיל של ה-Google Sheet שלך מהדפדפן:
-const REGULAR_SHEET_URL = "הדביקי_כאן_את_הקישור_הרגיל_של_הגיליון_מהדפדפן";
+const REGULAR_SHEET_URL = "https://docs.google.com/spreadsheets/d/1-FSsI60tnB40x1p-9S1qAJLdFW8cdAYoc_NjYdGgANs/edit?gid=1774263604#gid=1774263604";
 
 // 2. פונקציה שמחלצת את מזהה הגיליון ומייצרת קישור עוקף חסימות דפדפן (CORS)
 function getCleanDataUrl(url) {
